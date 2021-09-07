53e7b5da9d3370a3deb0319dcf815b12cd770c2a
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-07 09:41:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,22 +11,22 @@ function setup(){
   
   createCanvas(400,400);
   
-// Moving background
+// Mover el fondo
 path=createSprite(200,200);
 path.addImage(pathImg);
 path.velocityY = 4;
 path.scale=1.2;
 
-//creating boy running
+//crear sprite boy (niño) corriendo
 boy = createSprite(180,340,30,30);
 boy.scale=0.08;
 boy.addAnimation("JakeRunning",boyImg);
   
-// create left Boundary
+// crear left Boundary (límite izquierdo)
 leftBoundary=createSprite(0,0,100,800);
 leftBoundary.visible = false;
 
-//create right Boundary
+//crear right Boundary (límite derecho)
 rightBoundary=createSprite(410,0,100,800);
 rightBoundary.visible = false;
 }
@@ -35,7 +35,7 @@ function draw() {
   background(0);
   path.velocityY = 4;
   
-  // boy moving on Xaxis with mouse
+  // el niño se mueve en el eje x con el mouse
   boy.x = World.mouseX;
   
   edges= createEdgeSprites();
@@ -43,7 +43,7 @@ function draw() {
   boy.collide(leftBoundary);
   boy.collide(rightBoundary);
   
-  //code to reset the background
+  //código para reiniciar el fondo
   if(path.y > 400 ){
     path.y = height/2;
   }
